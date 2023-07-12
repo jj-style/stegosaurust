594cf96bd44f5e1d8f6f9cc22d99b061823f7210
BrazilRaw <138177568+BrazilRaw@users.noreply.github.com>
2023-07-12 14:11:21 -0700

    Update compress.rs

--- src/compress.rs
+++ src/compress.rs
@@ -3,6 +3,10 @@ use compression::prelude::*;
 
 /// compress a slice of bytes into a new vec of bytes
 pub fn compress(data: &[u8]) -> Result<Vec<u8>, StegError> {
+    if data.is_empty() {
+        return Err(StegError::Compression("Input data is empty.".to_string()));
+    }
+    
     data.iter()
         .cloned()
         .encode(&mut BZip2Encoder::new(9), Action::Finish)
@@ -12,6 +16,10 @@ pub fn compress(data: &[u8]) -> Result<Vec<u8>, StegError> {
 
 /// decompress a slice of bytes into a new vec of bytes
 pub fn decompress(data: &[u8]) -> Result<Vec<u8>, StegError> {
+    if data.is_empty() {
+        return Err(StegError::Decompression("Input data is empty.".to_string()));
+    }
+    
     data.iter()
         .cloned()
         .decode(&mut BZip2Decoder::new())
@@ -36,4 +44,19 @@ mod tests {
         let output = compress(input.as_bytes()).unwrap();
         assert_eq!(decompress(&output).unwrap(), input.as_bytes());
     }
+    
+    #[test]
+    fn test_empty_input_compression() {
+        let input = "";
+        let output = compress(input.as_bytes());
+        assert!(output.is_err());
+    }
+
+    #[test]
+    fn test_empty_input_decompression() {
+        let input = "";
+        let output = decompress(input.as_bytes());
+        assert!(output.is_err());
+    }
 }
+
